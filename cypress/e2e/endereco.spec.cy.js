c12d892365f9ac6f40a2b28295b8d6eb2187f2b6
Liandi <douglasliandi@hotmail.com>
2022-10-09 11:24:09 +0200

    organizando usando fixture

--- cypress/e2e/endereco.spec.cy.js
+++ cypress/e2e/endereco.spec.cy.js
@@ -8,6 +8,7 @@ describe('Funcionalidade Enderecos- Futuramento e Entrega', () => {
 
     beforeEach(() => {
         cy.visit('minha-conta')
+        
         cy.fixture('perfil').then (dados =>
         cy.login(dados.usuario, dados.senha))
     });
